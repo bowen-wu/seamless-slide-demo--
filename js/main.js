7edebcce8295690dfd54efa0ca1e5e5568127cbe
bowen-wu <13793248162@163.com>
2018-01-10 23:45:08 +0800

    状态机

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-window.onload = function(){   
-   
-}
+$(() => {
+    $pics = $('.window > .wrapper > img')
+    let n = 0
+    setInterval(() => {
+        leaveStatus(getNode()).one('transitionend',(event) => {
+            enterStatus($(event.currentTarget))
+        })
+        n++
+        currentStatus(getNode())
+    },2000)
+
+    // 工具函数
+    function getNode(){
+        return $pics.eq(n % $pics.length)
+    }
+    function leaveStatus($node){
+        return $node.removeClass('current').addClass('leave')
+    }
+    function enterStatus($node){
+        $node.removeClass('leave').addClass('enter')
+    }
+    function currentStatus($node){
+        $node.removeClass('enter').addClass('current')
+    }
+})
